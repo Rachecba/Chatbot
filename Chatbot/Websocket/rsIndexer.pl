754d8fbe66627755cb49bebee343031d61a9a750
unknown <sergioajm2909@gmail.com>
2019-10-10 00:48:24 -0600

    Integrated admin page with prolog server and WS

---
+++ Chatbot/Websocket/rsIndexer.pl
@@ -0,0 +1,26 @@
+/* 
+    Nombre: Ramson Gu Wu 
+    Id: 207440006 
+    Grupo # 2
+    Horario: 1pm
+*/
+:- module(rsIndexer, [
+                       reset_all_indexes/0,
+                       reset_some_indexes/1,
+                       next_index/2
+                       
+                     ]).
+:- dynamic index/2.
+reset_all_indexes :-
+    retractall(index(_ ,_))
+.
+reset_some_indexes(L) :-
+    forall(member(I, L), reset_index(I))
+.
+reset_index(I) :- reset_index(I, 1)
+.
+reset_index(I, V):- retractall(index(I, _)), assert(index(I, V))
+.
+
+next_index(I, V0) :- retract(index(I, V0)), V is V0 + 1, assert(index(I, V))
+.
